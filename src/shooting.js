66486420ce06907e911a832dd06b3fe89e339d70
spacemandev <devbharel@gmail.com>
2020-06-16 16:38:14 -0500

    updated with florad fixes

--- src/shooting.js
+++ src/shooting.js
@@ -1,25 +1,39 @@
 //Uses the selected actor to figure out guns
 let selected = canvas.tokens.controlled[0].actor;
+if (typeof selected === "undefined") {
+  ui.notifications.warn("You have no Token selected");
+}
+
 //Ignores melee/wepons that don't have the 'shots' property
-let weapons = selected.items.filter(el => el.type == "weapon" && el.data.data.shots > 0);
+let weapons = selected.items.filter(
+  (el) => el.type == "weapon" && el.data.data.shots > 0
+);
+
 //SWADE rules for how much ammo is expended per RoF
 const rofAmmo = { 1: 1, 2: 5, 3: 10, 4: 20, 5: 40, 6: 50 };
+
 //Dialog Creater that asks for any modifiers and what gun to use
 getFiringSolution();
+
 //Utility function for printing things to chat
 function printMessage(message) {
-    ChatMessage.create({
-        speaker: {
-            actor: selected,
-            alias: selected.name
-        },
-        content: message
-    }, {});
+  ChatMessage.create(
+    {
+      speaker: {
+        actor: selected,
+        alias: selected.name,
+      },
+      content: message,
+    },
+    {}
+  );
 }
+
 //Someone please teach me how to make pretty HTML templates because I can't CSS if my life depended on it
 function getFiringSolution() {
-    let weaponsList = getWeaponsListAsDropdown();
-    let template = `
+  let weaponsList = getWeaponsListAsDropdown();
+
+  let template = `
     <div>
       <div class="form-group">
         <label>Select Weapon</label>
@@ -57,97 +71,125 @@ function getFiringSolution() {
       </div>
     </div>
   `;
-    new Dialog({
-        title: "Ranged Attack",
-        content: template,
-        buttons: {
-            ok: {
-                label: "Fire Weapon",
-                callback: async (html) => {
-                    fireWeapon(html);
-                },
-            },
-            cancel: {
-                label: "Cancel"
-            }
+
+  new Dialog({
+    title: "Ranged Attack",
+    content: template,
+    buttons: {
+      ok: {
+        label: "Fire Weapon",
+        callback: async (html) => {
+          fireWeapon(html);
         },
-        default: "ok"
-    }).render(true);
+      },
+      cancel: {
+        label: "Cancel",
+      },
+    },
+    default: "ok",
+  }).render(true);
 }
+
 //utility function that that creates a dropdown of all weapons
 function getWeaponsListAsDropdown() {
-    let template = ``;
-    weapons.forEach((wep) => {
-        template += `<option value="${wep.name}">${wep.name} | ROF ${wep.data.data.rof} | Shots ${wep.data.data.shots} </option>`;
-        console.log(template);
-    });
-    return template;
+  let template = ``;
+  weapons.forEach((wep) => {
+    template += `<option value="${wep.name}">${wep.name} | ROF ${wep.data.data.rof} | Shots ${wep.data.data.shots} </option>`;
+    console.log(template);
+  });
+
+  return template;
 }
+
 //Computes the rolls, total modifier, and takes care of ammo
 function fireWeapon(html) {
-    let weapon = weapons.find(el => el.name == html.find("#selectedRangedWeapon")[0].value);
-    //swade pg93
-    let numShootingDie = html.find("#selectedRoF")[0].value;
-    //check if enough Ammo in clip to fire AND if RoF < weapon's RoF
-    if (numShootingDie > weapon.data.data.rof || numShootingDie < 1) {
-        printMessage("Selected RoF beyond Weapon RoF");
-        return;
-    }
-    if (rofAmmo[numShootingDie] > weapon.data.data.shots) {
-        printMessage(`Not enough Ammo to fire at this RoF. You only have (${weapon.data.data.shots}) shots left`);
-        return;
-    }
-    let shootingSkill = selected.items.find(el => el.name == "Shooting").data.data;
-    //individually rolls each die and explodes it. as per swade rules, each die is a seperate attack
-    let shootingRolls = [];
-    for (let i = 0; i < numShootingDie; i++) {
-        let newRoll = new Die(shootingSkill.die.sides).roll(1);
-        console.log(`Shooting Roll (${i}): `, newRoll.total);
-        let explodedRoll = newRoll.explode([shootingSkill.die.sides]).total;
-        console.log(`Shooting Roll (${i}) After Explosions: `, explodedRoll);
-        shootingRolls.push(explodedRoll);
-    }
-    //will roll wild die for all attacks but only shows it if selected actor is a WildCard
-    let wilddieRoll = new Die(shootingSkill['wild-die'].sides).roll(1).explode([shootingSkill['wild-die'].sides]).total;
-    console.log("Shooting Rolls Before Mod: ", shootingRolls);
-    console.log("Wild Die Roll Before Mod: ", wilddieRoll);
-    //Build the Modifiers
-    // Base Shooting Skill Mod
-    let shootingSkillMod = (isNaN((parseInt(shootingSkill.die.modifier))) ? 0 : parseInt(shootingSkill.die.modifier));
-    let coverMod = parseInt(html.find("#targetCover")[0].value);
-    let recoilMod = parseInt(html.find("#recoilPenalty")[0].value);
-    let multiActionMod = parseInt(html.find("#mapPenalty")[0].value);
-    let rangeMod = parseInt(html.find("#rangePenalty")[0].value);
-    let otherMod = parseInt(html.find("#otherMod")[0].value);
-    let distractedMod = ((selected.data.data.status.isDistracted) ? -2 : 0);
-    let woundMod = (selected.data.data.wounds.value - selected.data.data.wounds.ignored) * -1;
-    if (woundMod < -3) {
-        woundMod = -3;
-    } //swade pg95
-    let totalMod = shootingSkillMod + coverMod + recoilMod + multiActionMod + rangeMod + otherMod + distractedMod + woundMod;
-    console.log("Total Mod: ", totalMod);
-    //go over each roll and add the totalmod to it
-    shootingRolls.map(roll => { return roll += totalMod; });
-    wilddieRoll += totalMod;
-    console.log("Shooting Rolls: ", shootingRolls);
-    console.log("Wild Die Roll: ", wilddieRoll);
-    //Spend the Bullets
-    //selected.items.get(weapon.key).data.data.shots -= rofAmmo[numShootingDie]
-    selected.items.find(el => el.data.name == weapon.data.name).data.data.shots -= rofAmmo[numShootingDie];
-    //Print the result
-    let weaponDataTemplate = `
-  Weapon: ${weapon.data.name}
-  Notes: ${weapon.data.data.notes}
-  Shots Left: ${weapon.data.data.shots}  
-  `;
-    let weaponRollsTemplate = `
-  Total Modifier: ${totalMod}
-  Shooting Rolls: [${shootingRolls}]
-  `;
-    if (selected.data.data.wildcard) {
-        weaponRollsTemplate += `
-    Wild Die Roll: ${wilddieRoll}
-    `;
-    }
-    printMessage(weaponDataTemplate + weaponRollsTemplate);
+  let weapon = weapons.find(
+    (el) => el.name == html.find("#selectedRangedWeapon")[0].value
+  );
+
+  //swade pg93
+  let numShootingDie = html.find("#selectedRoF")[0].value;
+
+  //check if enough Ammo in clip to fire AND if RoF < weapon's RoF
+  if (numShootingDie >= weapon.data.data.rof || numShootingDie < 1) {
+    ui.notifications.warn("Selected RoF beyond Weapon RoF");
+    return;
+  }
+  if (rofAmmo[numShootingDie] > weapon.data.data.shots) {
+    ui.notifications.warn(`Not enough Ammo to fire at this RoF. You only have (${weapon.data.data.shots}) shots left`);
+    return;
+  }
+
+  let shootingSkill = selected.items.find((el) => el.data.name == "Shooting");
+  console.log(shootingSkill);
+  //individually rolls each die and explodes it. as per swade rules, each die is a seperate attack
+  let shootingRolls = [];
+  for (let i = 0; i < numShootingDie; i++) {
+    let newRoll = new Die(shootingSkill.data.data.die.sides).roll(1);
+    console.log(`Shooting Roll (${i}): `, newRoll.total);
+    newRoll = newRoll.explode([shootingSkill.data.data.die.sides]).total;
+    console.log(`Shooting Roll (${i}) After Explosions: `, newRoll);
+    shootingRolls.push(newRoll);
+  }
+
+  //will roll wild die for all attacks but only shows it if selected actor is a WildCard
+  let wilddieRoll = new Die(shootingSkill.data.data["wild-die"].sides)
+    .roll(1)
+    .explode([shootingSkill.data.data["wild-die"].sides]).total;
+
+  console.log("Shooting Rolls Before Mod: ", shootingRolls);
+  console.log("Wild Die Roll Before Mod: ", wilddieRoll);
+
+  //Build the Modifiers
+  // Base Shooting Skill Mod
+  let shootingSkillMod = isNaN(parseInt(shootingSkill.data.data.die.modifier))
+    ? 0
+    : parseInt(shootingSkill.data.data.die.modifier);
+  let coverMod = parseInt(html.find("#targetCover")[0].value);
+  let recoilMod = parseInt(html.find("#recoilPenalty")[0].value);
+  let multiActionMod = parseInt(html.find("#mapPenalty")[0].value);
+  let rangeMod = parseInt(html.find("#rangePenalty")[0].value);
+  let otherMod = parseInt(html.find("#otherMod")[0].value);
+
+  let distractedMod = selected.data.data.status.isDistracted ? -2 : 0;
+  let woundMod =
+    (selected.data.data.wounds.value - selected.data.data.wounds.ignored) * -1;
+  if (woundMod < -3) {
+    woundMod = -3;
+  } //swade pg95
+
+  let totalMod =
+    shootingSkillMod +
+    coverMod +
+    recoilMod +
+    multiActionMod +
+    rangeMod +
+    otherMod +
+    distractedMod +
+    woundMod;
+  console.log("Total Mod: ", totalMod);
+
+  //go over each roll and add the totalmod to it
+  shootingRolls.map((roll) => {
+    return (roll += totalMod);
+  });
+  wilddieRoll += totalMod;
+
+  console.log("Shooting Rolls: ", shootingRolls);
+  console.log("Wild Die Roll: ", wilddieRoll);
+
+  //Spend the Bullets
+  //selected.items.get(weapon.key).data.data.shots -= rofAmmo[numShootingDie]
+  let newShots = weapon.data.data.shots -= rofAmmo[numShootingDie]
+  weapon.update({'data.shots':  newShots});
+
+  let chatTemplate = `
+    <p>Weapon: ${weapon.data.name}</p>
+    <p>Notes: ${weapon.data.data.notes}</p>
+    <p>Shots Left: ${weapon.data.data.shots}</p>
+    <p>Total Modifier: ${totalMod}</p>
+    <p>Shooting Rolls: [${shootingRolls}]
+    ${(selected.data.data.wildcard) ? `<p>Wild Die Roll: ${wilddieRoll}</p>` : ""}
+  `
+  printMessage(chatTemplate);
 }
