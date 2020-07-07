da4597a7f04c66d0c1567a6024e2b3b8de5a02d8
nnayl <yann.lauvergne@gmail.com>
2020-07-08 01:00:51 +0200

    fixe wrong module settings

--- scripts/init.js
+++ scripts/init.js
@@ -4,7 +4,7 @@ function i18n(key) {
 
 Hooks.once("init", () => {
 
-    game.settings.register("swade-macro-simple", "skillShooting", {
+    game.settings.register("swade-macros-simple", "skillShooting", {
 		name: i18n("swadeMacro.combatFlow.settings.skillShooting.name"),
 		hint: i18n("swadeMacro.combatFlow.settings.skillShooting.hint"),
 		scope: "world",
@@ -13,7 +13,7 @@ Hooks.once("init", () => {
 		type: String
     });
     
-    game.settings.register("swade-macro-simple", "skillFighting", {
+    game.settings.register("swade-macros-simple", "skillFighting", {
 		name: i18n("swadeMacro.combatFlow.settings.skillFighting.name"),
 		hint: i18n("swadeMacro.combatFlow.settings.skillFighting.hint"),
 		scope: "world",
@@ -22,7 +22,7 @@ Hooks.once("init", () => {
 		type: String
     });
     
-    game.settings.register("swade-macro-simple", "trackAmmoConsumption", {
+    game.settings.register("swade-macros-simple", "trackAmmoConsumption", {
 		name: i18n("swadeMacro.combatFlow.settings.trackAmmo.name"),
 		hint: i18n("swadeMacro.combatFlow.settings.trackAmmo.hint"),
 		scope: "world",
@@ -31,7 +31,7 @@ Hooks.once("init", () => {
 		type: Boolean
 	});
     
-    game.settings.register("swade-macro-simple", "grittyDamage", {
+    game.settings.register("swade-macros-simple", "grittyDamage", {
 		name: i18n("swadeMacro.combatFlow.settings.grittyDamage.name"),
 		hint: i18n("swadeMacro.combatFlow.settings.grittyDamage.hint"),
 		scope: "world",
