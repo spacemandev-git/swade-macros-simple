a2c6115e0c4d6d62cdcd526eb07e77f031d63263
Bruno Calado <brunocalado@users.noreply.github.com>
2020-07-21 14:15:23 -0300

    dogfight and scale cards ok
    dogfight and scale cards ok

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # SWADE Macros Simple
 A set of useful macros for the SWADE system. Growing list, currently:
-### Card Draw Macro (Spacemandev)
+### Card Draw Macro (Spacemandev/brunocalado)
 Draws random cards as tiles on top of the page.
 
-**Options**
-- Reset?: Before draw the cards the table will be reset. 
-- Chase?: This option put the cards side by side. Unckeck it and the cards will be stacked.
+#### Options
+-  **Reset Table?:** Before draw the cards the table will be reset. Uncheck this to keep drawing cards without repeat.
+- **Just stack up all cards:** This option stacks all cards.
+- **Horizontal/vertical spacing:**: You can set how much space will be between the cards.
 
-**TODO**
-- Dog fighting 
+#### Example Chase
+
+#### Example Dogfight
 
 ### Adventure Card Macro (lipefl)
 Macro for giving adventure cards to characters with a prompt similar to Roll20 (you'll need a rollable table with the cards and a item to hold the cards)
--- src/drawCardsToTable.js
+++ src/drawCardsToTable.js
@@ -18,21 +18,31 @@ function getRequirements() {
 
   let template = `
   <p>Table to Draw From: <select id="tableName">${cardsList}</select></p>
-  <p>Number of Cards to Draw: <input id="drawAmt" type="number" style="width: 50px;" value=9></p>
   <p>
-    Height: <input id="height" type="number" style="width: 50px" value=150>
-    Width: <input id="width" type="number" style="width: 50px" value=107>
+    Cards to Draw (Lines x Columns): <input id="dogFightLines" type="number" min="1" style="width: 50px;" value=1> x <input id="dogFightColumns" type="number" min="1" style="width: 50px;" value=9>
+  </p>  
+  <p>
+    Height: <input id="height" type="number" min="1" style="width: 50px" value=150>
+    Width: <input id="width" type="number" min="1" style="width: 50px" value=107>
   </p>
   <br />
-  <label>
-    <input type="checkbox" id="chase" checked/>
-    Chase?
-  </label>  
-  <br />
-  <label>
+  <p>
     <input type="checkbox" id="reset" checked/>
     Reset Table?
-  </label>  
+  </p> 
+  <p>
+    <input type="checkbox" id="stackupcards"/>
+    Just stack up all cards
+  </p>  
+  <br />  
+  <p>
+    <h3>Horizontal spacing between cards</h3>
+    <input id="spacingx" type="range" min="0" max="100" value="20" step="5">
+  </p>
+  <p>
+    <h3>Vertical spacing between cards (dogfight only)</h3>
+    <input id="spacingy" type="range" min="0" max="100" value="100" step="5">  
+  </p>
   `;
   new Dialog({
     title: "Draw Cards To Table",
@@ -51,11 +61,15 @@ function getRequirements() {
   }).render(true);
 }
 
-async function makeTiles(html) {
+async function makeTiles(html) {  
+  let spacingx = html.find("#spacingx")[0].value/100;
+  let spacingy = html.find("#spacingy")[0].value/100;
+  let dogFightLines = html.find("#dogFightLines")[0].value;
+  let dogFightColumns = html.find("#dogFightColumns")[0].value;  
   let resetTable = html.find("#reset")[0].value;
-  let chase = html.find("#chase")[0].value;
+  const stackupcards = html.find("#stackupcards")[0].checked;
   let tableName = html.find("#tableName")[0].value;
-  let cardsToDraw = html.find("#drawAmt")[0].value;
+  let cardsToDraw = dogFightLines*dogFightColumns;
   let _height = html.find("#height")[0].value;
   let _width = html.find("#width")[0].value;
 
@@ -73,17 +87,28 @@ async function makeTiles(html) {
   let centerY = game.scenes.active.data.height / 2;
   
   let deltaX = 0;
+  let deltaY = 0;
+  let counter = 0;
   
-  for (let i = 0; i < cardsToDraw; i++) {    
-    if (chase=='on' && i!=0 ) {
-      deltaX = ( _width*i  );      
-    }
-    await Tile.create({
-      img: cardDraws[i].img,
-      width: _width,
-      height: _height,
-      x: centerX + deltaX,
-      y: centerY,
-    });    
-  }  
+  //console.log(spacingx + '/' + spacingy + ' stackupcards:' + (stackupcards!='on'));
+  for (let y = 0; y < dogFightLines; y++) {
+    deltaY = ( _height*y + _height*spacingy*y );
+    for (let x = 0; x < dogFightColumns; x++) {                
+      deltaX = ( _width*x + _width*spacingx*x );            
+      if (stackupcards) {
+        deltaX = 0;
+        deltaY = 0;        
+      }
+      await Tile.create({
+        img: cardDraws[counter].img,
+        width: _width,
+        height: _height,
+        x: centerX + deltaX,
+        y: centerY + deltaY
+      });      
+      //console.log('x:' + x + ' y:' + y + ' counter:' + counter + ' deltaX:' + deltaX + ' deltaY:' + deltaY);
+      counter = counter + 1;
+      //console.log('centerX: ' + centerX + ' / deltaX: ' + deltaX + ' / centerX+deltaX:' + (centerX+deltaX) ); 
+    }      
+  }
 }
