edca7df5786a793ff3ded96478a43132936e8255
Kirlian Silvestre <kirlian.silvestre@gmail.com>
2021-08-29 19:46:51 -0300

    update the macro to work currently.
    Updating the macro to work with the rolls' asynchronous Promise system

--- src/adventurecard.js
+++ src/adventurecard.js
@@ -12,92 +12,89 @@ let optionchars = "";
 var allchars = [];
 
 for (const char of chars) {
-  let myitem = char.items.find((i) => i.name === itemCard);
-  if (myitem !== null) {
-    /// filters the ones that has the item
-    optionchars += `<option value="` + char._id + `">` + char.name + `</option>`;
-    allchars.push(char._id);
-  }
+    let myitem = char.items.find((i) => i.name === itemCard);
+    if (myitem !== null) {
+        /// filters the ones that has the item
+        optionchars += `<option value="` + char.id + `">` + char.name + `</option>`;
+        allchars.push(char.id);
+    }
 }
 
 if (!optionchars) {
-  /// no chars
-  ui.notifications.warn(`No character has the item ` + itemCard + `.`, {});
+    /// no chars
+    ui.notifications.warn(`No character has the item ` + itemCard + `.`, {});
 }
 
 let template =
-  `<p>How many cards? <input type="number" value="1" id="qtde" style="width:50px" /></p>
+    `<p>How many cards? <input type="number" value="1" id="qtde" style="width:50px" /></p>
 <p>For wich character? <select id="jogs"><option value="todos">All</option>` +
-  optionchars +
-  `</select></p>`;
+    optionchars +
+    `</select></p>`;
 new Dialog({
-  title: "Give Adventure Cards",
-  content: template,
-  buttons: {
-    ok: {
-      label: "Give",
-      callback: function (html) {
-        applyFormOptions(html);
-      },
+    title: "Give Adventure Cards",
+    content: template,
+    buttons: {
+        ok: {
+            label: "Give",
+            callback: function (html) {
+                applyFormOptions(html);
+            },
+        },
+        cancel: {
+            label: "Cancel",
+        },
     },
-    cancel: {
-      label: "Cancel",
-    },
-  },
 }).render(true);
 
 function drawFromTable(tableName) {
-  /// thanks to Forien for this. Check his modules https://foundryvtt.com/community/forien
-  const table = game.tables.getName(tableName);
-  if (!table) {
-    ui.notifications.warn(`Table ${tableName} not found.`, {});
-    return;
-  }
-  let results = table.roll().results;
+    /// thanks to Forien for this. Check his modules https://foundryvtt.com/community/forien
+    const table = game.tables.getName(tableName);
 
-  // if table is without replacemenets, mark results as drawn
-  if (table.data.replacement === false) {
-    results = results.map((r) => {
-      r.drawn = true;
-      return r;
+    if (!table) {
+        ui.notifications.warn(`Table ${tableName} not found.`, {});
+        return;
+    }
+    return table.roll().then((card) => {
+        let cardData = card.results[0].data;
+        let collection = cardData.collection;
+        return "<p>@Compendium[" + collection + "." + cardData.resultId + "]{" + cardData.text + "}</p>";
     });
-
-    table.updateEmbeddedEntity("TableResult", results);
-  }
-
-  return results;
 }
 
 function applyFormOptions(html) {
-  let qtde = html.find("#qtde")[0].value;
-  let selchar = html.find("#jogs")[0].value;
+    let qtde = html.find("#qtde")[0].value;
+    let selchar = html.find("#jogs")[0].value;
 
-  if (selchar === "todos") {
-    for (let i = 0; i < allchars.length; i++) {
-      giveCards(qtde, allchars[i]);
+    if (selchar === "todos") {
+        for (let i = 0; i < allchars.length; i++) {
+            giveCards(qtde, allchars[i]);
+        }
+    } else {
+        giveCards(qtde, selchar);
     }
-  } else {
-    giveCards(qtde, selchar);
-  }
 
-  let chatData = {
-    user: game.user._id,
-    speaker: ChatMessage.getSpeaker(),
-    content: "Adventure Cards given",
-  };
-  ChatMessage.create(chatData, {});
+    let chatData = {
+        user: game.user.id,
+        speaker: ChatMessage.getSpeaker(),
+        content: "Adventure Cards given",
+    };
+    ChatMessage.create(chatData, {});
 }
 
 function giveCards(howmany, actorId) {
-  let char = game.actors.get(actorId);
-  let myitem = char.items.find((i) => i.name === itemCard);
-  let updatedesc = "";
+    let char = game.actors.get(actorId);
+    let myitem = char.items.find((i) => i.name === itemCard);
+    let updatedesc = "";
 
-  for (let i = 1; i <= howmany; i++) {
-    let results = drawFromTable(rollTableName);
-    updatedesc +=
-      "<p>@Compendium[" + results[0].collection + "." + results[0].resultId + "]{" + results[0].text + "}</p>";
-  }
+    myitem.data.data.description = ""; 
+    myitem.update({ ["data.description"]: myitem.data.data.description });
 
-  myitem.update({ ["data.description"]: updatedesc });
+    for (let i = 1; i <= howmany; i++) {
+        drawFromTable(rollTableName).then((card) => {
+           updatedesc += card;
+           myitem.update({ ["data.description"]: myitem.data.data.description + " " + updatedesc });
+        });
+    }
+
+
 }
